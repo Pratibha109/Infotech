9a1665f9cdfe4171a1372e89415871ec37bd2b6d
Pratibha109 <ghodakepratibha10@gmail.com>
2026-03-28 23:59:41 +0530

    update

---
+++ frontend/src/components/LatestCollection.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { ShopContext } from '../context/ShopContext'
+import Title from './Title';
+import { useContext } from 'react';
+
+const LatestCollection = () => {
+    const { products } = useContext(ShopContext);
+    
+  return (
+    <div className='my-10'>
+      <div className='text-center py-8 text-3xl'>
+        <Title text1={'LATEST'} text2={'COLLECTIONS'} />
+        <p className='w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600'>Check out our latest collection of products</p>
+      </div>
+    </div>
+  )
+}
+
+export default LatestCollection
