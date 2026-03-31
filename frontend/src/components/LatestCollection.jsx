01ee04be482eddd4ea6d3db162ecd7c062bb23b5
Pratibha109 <ghodakepratibha10@gmail.com>
2026-03-31 16:44:15 +0530

    collection

--- frontend/src/components/LatestCollection.jsx
+++ frontend/src/components/LatestCollection.jsx
@@ -1,12 +1,18 @@
-import React from 'react'
+import React, { useEffect,useState } from 'react'
 import { ShopContext } from '../context/ShopContext'
 import Title from './Title';
 import { useContext } from 'react';
+import ProductItem from './ProductItem';
+
 
 const LatestCollection = () => {
     const { products } = useContext(ShopContext);
    // const [latestProducts, setLatestProducts] = React.useState([]);
      const [latestProducts, setLatestProducts] = useState([]);
+
+     useEffect(()=>{
+      setLatestProducts(products.slice(0,10));
+     },[])
     
   return (
     <div className='my-10'>
@@ -14,8 +20,17 @@ const LatestCollection = () => {
         <Title text1={'LATEST'} text2={'COLLECTIONS'} />
         <p className='w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600'>Check out our latest collection of products</p>
       </div>
+      {/* Rendaring products  */}
+      <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6'>
+        {
+          latestProducts.map((item,index)=>(
+            <ProductItem key={index} id={item._id} image={item.image} name={item.name} price={item.price} />
+          ))
+        }
+      </div>
     </div>
   )
 }
+
 
 export default LatestCollection
